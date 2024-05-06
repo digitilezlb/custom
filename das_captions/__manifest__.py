a3c9ccd030f6da541085156447d406363c8831d7
wassimakel <97348449+maysahdab@users.noreply.github.com>
2024-05-06 13:05:37 +0300

    das_caption

---
+++ das_captions/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Das Captions",
+    "version": "16.0.0.0",
+    "author": "Das360",
+    "website": "https://das-360.com",
+    "license": 'AGPL-3',
+    "auto_install": False,
+    "installable": True,
+    "depends": ["das_template_settings"],
+    "data": [
+        "views/das_captions.xml",
+        "data/data.xml",
+        'security/ir.model.access.csv',
+    ],
+    "application": True,
+}
