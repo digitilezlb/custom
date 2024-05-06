37422e165c84b7cd3a8b6702deff080a47db790e
wassimakel <97348449+maysahdab@users.noreply.github.com>
2024-05-06 12:53:56 +0300

    das_variant_discount

---
+++ das_variant_discount/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Variant Discount",
+    "description": "Variant Discount",
+    "summary": "Variant Discount",
+    "version": "16.0.1.0.1",
+    "author": "Das360",
+    "depends": ["base", "product","sale"],
+    "data": [
+        'views/price_list_variant.xml',
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'sequence': -100,
+    'application': True,
+    'auto_install': True,
+    "license": 'AGPL-3',
+    "website": "https://das-360.com",
+
+}
